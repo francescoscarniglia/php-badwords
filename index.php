d5848e18982c1b60b41ed097662895aef23f536d
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-05-15 19:48:35 +0200

    Ex.

--- index.php
+++ index.php
@@ -10,22 +10,50 @@
 
 <!-- START PHP -->
 <?php
-  $text = 'è uno studente della scuola Boolean';
-  $name = $_GET['name'];
-  // debug
-  //$color = 'blue';
-
+  $text = "è uno studente della scuola Boolean";
+  $id_name = $_GET['id_name'];
+  $words = explode(' ' , $text);
+  $lengthString = strlen($text);
+  $replaced = str_replace($id_name, '***', $id_name);
 ?><!-- END PHP -->
 
 <header>
-  <!-- debug -->
-  <!-- <p style="color:<?php  echo $color; ?>"> -->
+
+  <h2>Vars</h2>
   <p>
-    Francesco: <?php echo $text;?>
+  <?php echo ucfirst($id_name); ?> : <?php echo $text;?>
+  </p>
+
+  <h2> <?php echo ucfirst($id_name);?></h2>
+  <p>
+    <?php echo $words[0];?>
   </p>
   <p>
-    name: <?php echo $name ?>
+    <?php echo $words[1];?>
   </p>
+  <p>
+    <?php echo $words[2];?>
+  </p>
+  <p>
+    <?php echo $words[3];?>
+  </p>
+
+  <p>
+    <?php echo $words[4];?>
+  </p>
+
+  <p>
+    <?php echo $words[5];?>
+  </p>
+
+  <p>
+    La lunghezza del paragrafo è: <strong> <?php echo $lengthString;?> </strong>
+  </p>
+
+  <p>
+    Sotituzione stringa : <?php echo $replaced;?>
+  </p>
+
 </header>
 
 </body>
